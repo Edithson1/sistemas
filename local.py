b2c89eb700b38d75bc604227388830c49c64fabc
Edithson Aybar <152218004+Edithson1@users.noreply.github.com>
2024-07-13 17:48:19 -0500

    Update local.py

--- local.py
+++ local.py
@@ -6,23 +6,26 @@ from io import StringIO
 # Definir la interfaz de Streamlit
 st.title('Modificación de Archivo CSV en GitHub')
 
+# URL del archivo CSV en GitHub
+url = 'https://raw.githubusercontent.com/tu_usuario/tu_repositorio/main/datos.csv'
+
 # Función para descargar y modificar el archivo CSV
 def modificar_csv(numero):
     # Descargar el archivo CSV y leerlo
-
+    response = requests.get(url)
     if response.status_code == 200:
         # Convertir la respuesta a un StringIO para que Pandas pueda leerlo
-        df = pd.read_csv('https://github.com/Edithson1/sistemas/blob/main/generated_data.csv')
+        df = pd.read_csv(url)
         
         # Modificar el DataFrame
-        #nueva_fila = {'Numero': numero}
-        #df = df.append(nueva_fila, ignore_index=True)
+        nueva_fila = {'Numero': numero}
+        df = df.append(nueva_fila, ignore_index=True)
         
         # Guardar el DataFrame modificado en un nuevo archivo CSV localmente
-        #df.to_csv('datos_modificados.csv', index=False)
+        df.to_csv('datos_modificados.csv', index=False)
         
         # Mostrar mensaje de éxito
-        #st.success('Archivo CSV modificado y guardado localmente.')
+        st.success('Archivo CSV modificado y guardado localmente.')
     else:
         st.error(f'Error al descargar el archivo CSV. Código de estado: {response.status_code}')
 
